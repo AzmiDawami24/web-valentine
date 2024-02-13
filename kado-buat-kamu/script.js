1864c7505c10e3e2f80945024d44d17a60e0d5fb
Dawam <100006614+AzmiDawami24@users.noreply.github.com>
2024-02-13 21:48:13 +0700

    Add files via upload

---
+++ kado-buat-kamu/script.js
@@ -0,0 +1,14 @@
+document.getElementById("loveButton").addEventListener("click", function() {
+  var messages = [
+      alert("Hai, Afifah..."),
+      alert("Makasih ya udah nemenin aku sampe sekarang..."),
+      alert("Maaf yaa kalo aku sering bikin kamu marah & ngambek 🥺🥺🥺"),
+      alert("Aku bakal berusaha jadi yang terbaik buat kamu 🥰🥰🥰"),
+      alert("Maaf yaa kalo aku belum bisa kasih apa-apa di hari valentine ini 🥺🥺🥺"),
+      alert("Aku cuma bisa ngasih kamu website ini 🥺🥺🥺"),
+      alert("Jangan tinggalin aku yaa 🥺👉👈"),
+      alert("Karena aku sayang banget sama kamu 🥺🥺🥺"),
+      alert("Aku gamau kamu pergi 🥺🥺🥺"),
+      alert("I love you everytime babe ❤️❤️❤️"),
+  ];
+});
